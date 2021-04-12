8c66554c2eb2259fce4437f1ad755875ea3d00ae
Noel Welsh <noel@noelwelsh.com>
2021-04-12 23:39:41 +0100

    Set UTF-8 encoding when reading resources

--- data/src/main/scala/glomma/data/Reader.scala
+++ data/src/main/scala/glomma/data/Reader.scala
@@ -1,10 +1,10 @@
 package glomma.data
 
-import scala.io.Source
+import scala.io.{Codec, Source}
 
 /** Read lines of text from a file that is stored in JVM resource directory
   */
 object Reader {
   def read(fileName: String): List[String] =
-    Source.fromResource(fileName).getLines().toList
+    Source.fromResource(fileName)(Codec.UTF8).getLines().toList
 }
