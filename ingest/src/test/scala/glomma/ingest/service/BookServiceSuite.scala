f0e22923f2e1064a01fd197131753fe709c0489f
Noel Welsh <noel@untyped.com>
2021-04-22 20:14:24 +0100

    Add utility to sample some book events

--- ingest/src/test/scala/glomma/ingest/service/BookServiceSuite.scala
+++ ingest/src/test/scala/glomma/ingest/service/BookServiceSuite.scala
@@ -1,8 +1,12 @@
 package glomma.ingest.service
 
-import munit._
+import glomma.data.data.Scenarios
+import glomma.data.random._
 import glomma.event.Book
+import munit._
 
 class BookServiceSuite extends FunSuite {
-  def books(): List[Book] = ???
+  // Sample a selection of books from the small scenario
+  def books(): List[Book] =
+    Scenarios.small.sample().books.map(b => Book(b.name, b.price))
 }
